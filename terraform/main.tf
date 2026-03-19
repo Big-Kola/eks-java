8c5b81a6c6d4adc89f63582dd7735e55e217e0d0
Big-Kola <olakanlekolade100@gmail.com>
2026-03-19 11:30:26 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 terraform {
   required_version = ">= 0.12"
+
   backend "s3" {
     bucket = "myapp-tf-s3-bucket-us-east-2"
     key    = "terraform.tfstate"
@@ -14,23 +15,25 @@ provider "aws" {
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name: "${var.env_prefix}-vpc"
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
-    tags = {
-    Name: "${var.env_prefix}-subnet-1"
+
+  tags = {
+    Name = "${var.env_prefix}-subnet-1"
   }
 }
 
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
+
   tags = {
-    Name: "${var.env_prefix}-igw"
+    Name = "${var.env_prefix}-igw"
   }
 }
 
@@ -41,8 +44,9 @@ resource "aws_default_route_table" "main-rtb" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myapp-igw.id
   }
+
   tags = {
-    Name: "${var.env_prefix}-main-rtb"
+    Name = "${var.env_prefix}-main-rtb"
   }
 }
 
@@ -50,62 +54,61 @@ resource "aws_default_security_group" "default-sg" {
   vpc_id = aws_vpc.myapp-vpc.id
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "TCP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
     cidr_blocks = [var.my_ip, var.jenkins_ip]
   }
 
   ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "TCP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
     prefix_list_ids = []
   }
 
   tags = {
-    Name: "${var.env_prefix}-default-sg"
+    Name = "${var.env_prefix}-default-sg"
   }
 }
 
 data "aws_ami" "latest-amazon-linux-image" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
+
   filter {
-    name = "name" 
+    name   = "name"
     values = ["amzn2-ami-kernel-*-x86_64-gp2"]
   }
+
   filter {
-    name = "virtualization-type"
+    name   = "virtualization-type"
     values = ["hvm"]
   }
 }
 
 resource "aws_instance" "myapp-server" {
-  ami = data.aws_ami.latest-amazon-linux-image.id
-  instance_type = var.instance_type
-
-  subnet_id = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-  availability_zone = var.avail_zone
-
+  ami                         = data.aws_ami.latest-amazon-linux-image.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
+  availability_zone           = var.avail_zone
   associate_public_ip_address = true
-  key_name = "docker-server"
+  key_name                    = "myapp-key-pair-new"  # <-- Updated key pair
 
-  user_data = file("entry-script.sh")
-
-  user_data_replace_on_change = true
+  user_data                    = file("entry-script.sh")
+  user_data_replace_on_change  = true
 
   tags = {
-    Name: "${var.env_prefix}-server"
+    Name = "${var.env_prefix}-server"
   }
 }
 
